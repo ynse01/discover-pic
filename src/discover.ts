4845db18158b2bab761edf5735cc4f8c3b6b6b51
ynse <laptop@ynse.nl>
2020-06-05 16:51:39 +0200

    Use viewBox

--- src/discover.ts
+++ src/discover.ts
@@ -15,12 +15,12 @@ export class DiscoverThePicture {
             throw new Error(`Unable to find SVG element with ID: ${this.id}.`);
         }
         const svg = <SVGElement><any>element;
-        const width = (<any>svg)["viewBox"].baseVal.width;
-        const height = (<any>svg)["viewBox"].baseVal.height;
+        const width: number = (<any>svg)["viewBox"].baseVal.width;
+        const height: number = (<any>svg)["viewBox"].baseVal.height;
         if (width == null || height == null) {
-            throw new Error(`SVG Element doesn't have a size.`);
+            throw new Error(`SVG Element doesn't have a viewBox.`);
         }
-        this.grid = new Grid(parseInt(width), parseInt(height));
+        this.grid = new Grid(width, height);
         new GridView(svg, this.grid);
     }
 
