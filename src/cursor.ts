d7009a1820a161d4820dacc61682b087bfac99bf
ynse <laptop@ynse.nl>
2020-06-06 18:13:47 +0200

    Cusor position on center

--- src/cursor.ts
+++ src/cursor.ts
@@ -26,8 +26,8 @@ export class Cursor {
 
     private _drawCursor(): SVGRectElement {
         const cursor = document.createElementNS(GridView.svgNS, "rect");
-        cursor.setAttribute("x", `${this._grid.getXPos(this._xPos)}`);
-        cursor.setAttribute("y", `${this._grid.getYPos(this._yPos)}`);
+        cursor.setAttribute("x", `${this._grid.getXPos(this._xPos - 1)}`);
+        cursor.setAttribute("y", `${this._grid.getYPos(this._yPos - 1)}`);
         cursor.setAttribute("width", `${this._grid.cellWidth * 3}`);
         cursor.setAttribute("height", `${this._grid.cellHeight * 3}`);
         cursor.setAttribute("id", "cursor");
@@ -39,8 +39,8 @@ export class Cursor {
     private _moveCursor(): void {
         const cursor = document.getElementById("cursor");
         if (cursor !== null) {
-            cursor.setAttribute("x", `${this._grid.getXPos(this._xPos)}`);
-            cursor.setAttribute("y", `${this._grid.getYPos(this._yPos)}`);
+            cursor.setAttribute("x", `${this._grid.getXPos(this._xPos - 1)}`);
+            cursor.setAttribute("y", `${this._grid.getYPos(this._yPos - 1)}`);
         }
     }
 
@@ -53,33 +53,39 @@ export class Cursor {
         switch(args.key) {
             case "Down":
             case "ArrowDown":
-                if (this._yPos + 2 < this._grid.numRows) {
+                if (this._yPos + 1 < this._grid.numRows) {
                     this._yPos++;
                 }
                 break;
             case "Up":
             case "ArrowUp":
-                if (this._yPos >= 0) {
+                if (this._yPos > 0) {
                     this._yPos--;
                 }
                 break;
             case "Left":
             case "ArrowLeft":
-                if (this._xPos >= 0) {
+                if (this._xPos > 0) {
                     this._xPos--;
                 }
                 break;
             case "Right":
             case "ArrowRight":
-                if (this._xPos + 2 < this._grid.numCols) {
+                if (this._xPos + 1 < this._grid.numCols) {
                     this._xPos++;
                 }
                 break;
             case " ":
-                // For now, toggle the center cell.
-                this._grid.toggleStatus(this._xPos + 1, this._yPos + 1);
+                this._apply(this._xPos, this._yPos);
                 break;
         }
         this._moveCursor();
+    }
+
+    private _apply(x: number, y: number): void {
+        const hint = this._grid.getContent(x, y);
+        if (hint !== " ") {
+            this._grid.toggleStatus(x, y);
+        }
     }
 }
