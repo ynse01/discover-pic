122d51678a1a42e4583cd46af51b4f18a84c84c6
ynse <laptop@ynse.nl>
2020-07-31 11:53:26 +0200

    Update applied state also on clicking

--- src/game-clicker.ts
+++ src/game-clicker.ts
@@ -2,6 +2,8 @@ import { Clicker } from "./clicker.js";
 import { GridCell } from "./grid-cell.js";
 import { CellStatus } from "./grid.js";
 import { IGame } from "./i-game.js";
+import { MicroIterator } from "./micro-iterator.js";
+import { Block } from "./block.js";
 
 export class GameClicker extends Clicker {
     private _status: CellStatus | undefined;
@@ -22,7 +24,30 @@ export class GameClicker extends Clicker {
     }
 
     protected onStop(_cell: GridCell | undefined): void {
-        // Nothing to do
+        // Update applied status.
+        this._updateApplied();
     }
     
+    private _updateApplied(): void {
+        const blocks: Block[] = [];
+        if (this._cells !== undefined) {
+            this._cells.forEach(cell => {
+                const iterator = new MicroIterator(cell);
+                iterator.forEach(neighbor => {
+                    const block = this._game.grid.getBlock(neighbor);
+                    if (block !== undefined && !blocks.includes(block)) {
+                        blocks.push(block);
+                    }    
+                });
+            });
+        }
+        blocks.forEach(block => {
+            const previousState = block.applied;
+            const newState = block.checkApplied();
+            if (previousState !== newState) {
+                // Refresh the state
+                this._game.grid.setStatus(block.cell, this._game.grid.getStatus(block.cell));
+            }
+        });
+    }
 }
